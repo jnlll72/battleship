8f0c182c3d3f3457f9291baab4828827020647ac
Julien Le Gall <jnlll72@gmail.com>
2016-11-29 15:02:22 +0100

    maj 0.2

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,8 @@ public class Main {
         Boat b2 = new Boat(4, 2, 2, 1);
         Boat b3 = new Boat(3, 1, 1, 1);
 
+
+
         boolean r1 = grid.addBoat(b1);
         boolean r2 = grid.addBoat(b2);
         boolean r3 = grid.addBoat(b3);
